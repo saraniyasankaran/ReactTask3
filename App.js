53cf37f22c801542d0000874790eb7ce0c87330d
saraniyasankaran <119334289+saraniyasankaran@users.noreply.github.com>
2023-02-22 10:01:30 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Circle from "./Circle";
+import './index.css';
+import Triangle from "./Triangle";
+import Square from"./Square";
+import Perimeter from "./Perimeter";
+import Hexagon from "./Hexagon";
+import Cylinder from "./Cylinder";
+import Cone from "./Cone";
+import Task3 from "./Task3";
+function App(){
+  return(
+    <Task3/>
+   
+    
+  );
+
+}
+export default App;
